039316f6ae7120917e597e4fdf8ed1e3f51f1afe
friflaj <Emiliano.Heyns@iris-advies.nl>
2009-10-15 13:07:07 +0200

    Wiki import

--- topost.py
+++ topost.py
@@ -6,8 +6,8 @@ import os
 
 for page in glob.glob('_posts/*.markdown'):
     newpage = page[len('_posts/'):]
-    if re.match('[0-9]{4}-[0-9]{2}-[0-9]{2}-', page):
-        newpage = page[11:]
+    if re.match('[0-9]{4}-[0-9]{2}-[0-9]{2}-', newpage):
+        newpage = newpage[11:]
     modified = time.strftime("%Y-%m-%d",time.localtime(os.path.getmtime(page)))
 
     newpage = '_posts/' + modified + '-' + newpage
